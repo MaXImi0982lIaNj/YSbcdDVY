ef028861de0956e13d5400c38b8a9cc0faca5c45
MaXImi0982lIaNj <hurrahs_pour.00@icloud.com>
2025-09-03 03:30:41 +0900

    Create JS 密码加密解密工具

---
+++ password_encryption_decryption_tool_0903_0230_czh.svelte
@@ -0,0 +1,46 @@
+// 代码生成时间: 2025-09-03 02:30:40
+  // 引入 crypto 模块用于密码加密和解密
+  import { createHash } from 'crypto';
+</script>
+
+<script>
+  // 定义状态变量
+  let password = '';
+  let hashedPassword = '';
+  let decryptedPassword = '';
+  let errorMessage = '';
+
+  // 加密密码函数
+  function encryptPassword() {
+    try {
+      // 使用 SHA-256 算法加密密码
+      const hash = createHash('sha256');
+      hash.update(password);
+      hashedPassword = hash.digest('hex');
+      errorMessage = '';
+    } catch (error) {
+      errorMessage = 'Error encrypting password: ' + error.message;
+# FIXME: 处理边界情况
+    }
+  }
+
+  // 解密密码函数
+  // 由于密码是单向加密，这里仅作为展示，实际无法解密
+  function decryptPassword() {
+# TODO: 优化性能
+    errorMessage = 'Decryption not possible for SHA-256 encrypted passwords.';
+  }
+</script>
+
+<!-- 显示密码加密解密界面 -->
+<div>
+  <input type="text" placeholder="Enter password" on:input="password = event.target.value" />
+  <button on:click="encryptPassword">Encrypt</button>
+  <button on:click="decryptPassword">Decrypt</button>
+  <p>Hashed Password: {hashedPassword}</p>
+  <p>Decrypted Password: {decryptedPassword}</p>
+  {#if errorMessage}
+    <p style="color: red;">{errorMessage}</p>
+  {/if}
+# NOTE: 重要实现细节
+</div>
