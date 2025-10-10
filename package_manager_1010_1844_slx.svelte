7c71c931a68521ff14c091c49bab3d4cbb534b39
MaXImi0982lIaNj <hurrahs_pour.00@icloud.com>
2025-10-10 19:44:44 +0900

    Update JS 软件包管理器

---
+++ package_manager_1010_1844_slx.svelte
@@ -0,0 +1,31 @@
+// 代码生成时间: 2025-10-10 18:44:43
+        // Define the PackageManager component
+        svelte.compile("<div>
+            <h1>Package Manager</h1>
+            <input type="text" bind:value={packageName} placeholder="Enter package name" />
+            <button on:click={installPackage}>Install</button>
+            <ul>
+                {#each installedPackages as package}
+                    <li>{package.name} - {package.version}</li>
+                {/each}
+            </ul>
+        </div>", {
+            vars: ['packageName', 'installedPackages'],
+            onrender: function () {
+                this.installedPackages = [];
+            },
+            installPackage: function () {
+                try {
+                    // Simulate package installation
+                    const packageVersion = '1.0.0'; // This should be dynamically fetched in real scenarios
+                    const packageData = { name: this.packageName, version: packageVersion };
+                    this.installedPackages.push(packageData);
+                    this.packageName = '';
+                } catch (error) {
+                    console.error('Error installing package:', error);
+                }
+            }
+        });
+    </script>
+</body>
+</html>
